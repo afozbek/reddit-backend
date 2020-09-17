e5cc16ad22e8da789d156eaf3aee8d0598e70d6b
afozbek <furkanozbek1995@gmail.com>
2020-09-17 21:37:31 +0300

    constantt güncellendi

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-export const __prod__ = process.env.NODE_ENV !== "production";
+export const __prod__ = process.env.NODE_ENV === "production";
 export const __DB_PASSWORD__ = process.env.DB_PASSWORD;
 export const __DB_USER__ = process.env.DB_USER;
 export const __DATABASE_TYPE__ = process.env.DATABASE_TYPE;
