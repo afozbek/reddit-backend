c0af4a9495b9435c32ebfac3d6feea12ca36f0de
afozbek <furkanozbek1995@gmail.com>
2020-09-17 21:16:39 +0300

    constantlar eklendi

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
 require("dotenv").config();
 
 export const __prod__ = process.env.NODE_ENV !== "production";
-export const __POSTGRE_PASSWORD__ = process.env.POSTGRE_PASSWORD;
+export const __DB_PASSWORD__ = process.env.DB_PASSWORD;
+export const __DB_USER__ = process.env.DB_USER;
+export const __DATABASE_TYPE__ = process.env.DATABASE_TYPE;
