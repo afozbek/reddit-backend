bb2e3ae4b07a8c436e98da3da854f0f81b1ebccc
afozbek <furkanozbek1995@gmail.com>
2020-09-17 21:17:09 +0300

    uygulama ayağa kaldırıldı

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-console.log("Hello World");
+import { MikroORM } from "@mikro-orm/core";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
 
-console.log("Furkan");
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(8080, () => {
+    console.log("Server is listening on 8080");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
