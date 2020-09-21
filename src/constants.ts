cb87897660c542b31b120e8fdd35b35d7ad92a17
afozbek <furkanozbek1995@gmail.com>
2020-09-21 18:32:10 +0300

    redis için forgot pass prefix oluşturuldu

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,4 @@ export const __DB_PASSWORD__ = process.env.DB_PASSWORD;
 export const __DB_USER__ = process.env.DB_USER;
 export const __DATABASE_TYPE__ = process.env.DATABASE_TYPE;
 export const __SESSION_COOKIE_NAME__ = "qid";
+export const __FORGET_PASSWORD_PREFIX__ = "forget-password:";
