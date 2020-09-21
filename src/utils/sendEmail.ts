0a07b7278104ecb5671be8e95e7411257f5628fe
afozbek <furkanozbek1995@gmail.com>
2020-09-21 17:54:39 +0300

    mail gönderme için nodemailer kullanıldı

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+
+// async..await is not allowed in global scope, must use a wrapper
+export async function sendEmail(to: string, html: string, subject: string) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = {
+    user: "vxhjoizout67ojgg@ethereal.email",
+    pass: "6rcQbdauzrRZJ2FHkD",
+  };
+  console.log(testAccount);
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to, // list of receivers
+    subject, // Subject line
+    html,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
