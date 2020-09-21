29eb538b52074cbd7bb15120f9e4fa35c1c307c9
afozbek <furkanozbek1995@gmail.com>
2020-09-21 17:54:09 +0300

    register için validation dosyası oluşturuldu

---
+++ src/utils/validateRegister.ts
@@ -0,0 +1,37 @@
+import { FieldError } from "src/resolvers/FieldError";
+import { UserInput } from "src/resolvers/UserInput";
+
+export const validateRegister = (options: UserInput) => {
+  // SIMPLE VALIDATIONS
+  const errors: FieldError[] = [];
+
+  if (options.username.trim().length < 5) {
+    errors.push({
+      field: "username",
+      message: "Username length must be at least 5 characters",
+    });
+  }
+
+  if (options.username.includes("@")) {
+    errors.push({
+      field: "username",
+      message: "Username cannot have '@' character",
+    });
+  }
+
+  if (options.password.trim().length < 5) {
+    errors.push({
+      field: "password",
+      message: "Password length must be at least 5 characters",
+    });
+  }
+
+  if (!options.email.includes("@")) {
+    errors.push({
+      field: "email",
+      message: "Please enter the email correctly",
+    });
+  }
+
+  return errors;
+};
