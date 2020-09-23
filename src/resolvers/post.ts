ffe9ffe4e7be387f84843b306b5acc16c5afa5ee
afozbek <furkanozbek1995@gmail.com>
2020-09-23 23:01:53 +0300

    cliente gönderilen textler slice edildi

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -4,11 +4,13 @@ import {
   Arg,
   Ctx,
   Field,
+  FieldResolver,
   InputType,
   Int,
   Mutation,
   Query,
   Resolver,
+  Root,
   UseMiddleware,
 } from "type-graphql";
 import { Post } from "./../entities/Post";
@@ -22,8 +24,13 @@ class PostInput {
   text: string;
 }
 
-@Resolver()
+@Resolver(Post)
 export class PostResolver {
+  @FieldResolver(() => String)
+  textSnippet(@Root() root: Post) {
+    return root.text.slice(0, 70);
+  }
+
   @Query(() => [Post])
   async posts(
     @Arg("limit", () => Int) limit: number,
