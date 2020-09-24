e8389b10f0e4e2bda78e645faf9595805c2e92f0
afozbek <furkanozbek1995@gmail.com>
2020-09-24 18:14:13 +0300

    email için fieldResolver oluşturuldu

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -4,10 +4,12 @@ import {
   Arg,
   Ctx,
   Field,
+  FieldResolver,
   Mutation,
   ObjectType,
   Query,
   Resolver,
+  Root,
 } from "type-graphql";
 
 import argon2 from "argon2";
@@ -37,8 +39,18 @@ const errorGenerate = (err: FieldError) => {
   return errObj;
 };
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+  @FieldResolver(() => String)
+  email(@Root() user: User, @Ctx() { req }: MyContext) {
+    if (req.session.userId === user.id) {
+      return user.email;
+    }
+
+    // current user wants to see other emails
+    return "";
+  }
+
   @Mutation(() => UserResponse)
   async changePassword(
     @Arg("newPassword") newPassword: string,
