fa4703a75cde24c062ea8c074b5ddf9d875e347e
afozbek <furkanozbek1995@gmail.com>
2020-09-25 22:42:14 +0300

    OnetoMany eklendi

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -5,9 +5,11 @@ import {
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { Updoot } from "./Updoot";
 import { User } from "./User";
 
 @ObjectType()
@@ -24,6 +26,9 @@ export class Post extends BaseEntity {
   @Field()
   @ManyToOne(() => User, (user) => user.posts)
   creator: User;
+
+  @OneToMany(() => Updoot, (updoot) => updoot.post)
+  updoots: Updoot[];
 
   @Field({ description: "Title of the post" })
   @Column()
