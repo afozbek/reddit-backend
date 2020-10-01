d393bb1a6d578bc42e079694abca59155bebd183
afozbek <furkanozbek1995@gmail.com>
2020-10-01 18:35:56 +0300

    loaderlar ctx e eklendi

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
+import { createUserLoader } from "./utils/CreateUserLoader";
+import { createUpdootLoader } from "./utils/CreateUpdootLoader";
 
 export type MyContext = {
   req: Request & { session: Express.Session };
   res: Response;
   redis: Redis;
+  userLoader: ReturnType<typeof createUserLoader>;
+  updootLoader: ReturnType<typeof createUpdootLoader>;
 };
